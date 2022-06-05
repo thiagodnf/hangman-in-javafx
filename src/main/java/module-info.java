1979a376a5953400cbe2387f6095917f11b5488a
Thiago do N. Ferreira <thiagodonferreira@gmail.com>
2022-06-05 19:29:54 -0400

    added module-info.java and other features

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module tdnf.hangmanfx {
+	
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+
+    requires static lombok;
+    
+    opens tdnf.hangmanfx to javafx.fxml;
+    opens tdnf.hangmanfx.controller;
+    
+    exports tdnf.hangmanfx;
+    exports tdnf.hangmanfx.controller;
+    exports tdnf.hangmanfx.model;
+    exports tdnf.hangmanfx.util;
+    
+}
