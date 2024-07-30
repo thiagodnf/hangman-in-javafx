f89f22d3eb7263f20227d696f4b1dc875509327f
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2024-07-29 23:56:05 -0400

    added new transitive keyword to remove warning

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,11 @@
-module tdnf.hangmanfx {
+module thiagodnf.hangmanfx {
 
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+    requires transitive javafx.media;
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
-
-    requires static lombok;
 
     opens thiagodnf.hangmanfx to javafx.fxml;
     opens thiagodnf.hangmanfx.controller;
